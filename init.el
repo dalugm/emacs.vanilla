9342935cb86d5693a2da9259dd94e9fb0f9932e5
dalu <mou.tong@outlook.com>
2023-06-24 14:36:44 +0800

    Update package.el configuration

--- init.el
+++ init.el
@@ -28,6 +28,10 @@
               ;; If x10, half of cpu time is spent on gc when scrolling.
               (setq gc-cons-threshold (* 100 old-gc-cons-threshold)))))
 
+(load
+ (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+ t t)
+
 (require 'init-utils)
 (require 'init-modeline)
 (require 'init-funcs)
@@ -59,11 +63,7 @@
 ;; Personal setup.
 (load (expand-file-name "~/.custom.el") t nil)
 
-(load
- (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
- t t)
-
-(message "*** Emacs loaded in %s with %d garbage collections. ***"
+(message "Emacs ready in %s with %d garbage collections."
          (format "%.2f seconds"
                  (float-time
                   (time-subtract after-init-time before-init-time)))
