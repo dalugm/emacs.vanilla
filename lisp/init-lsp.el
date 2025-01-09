e37dd891e22d8c581b66b670f3aafce4cc027565
dalu <mou.tong@outlook.com>
2025-01-09 10:56:21 +0800

    Update lsp keybindings.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -12,11 +12,15 @@
 (with-eval-after-load 'eglot
   (global-set-key (kbd "C-c l a") #'eglot-code-actions)
   (global-set-key (kbd "C-c l c") #'eglot-show-workspace-configuration)
-  (global-set-key (kbd "C-c l d") #'eldoc)
+  (global-set-key (kbd "C-c l d") #'eglot-find-declaration)
   (global-set-key (kbd "C-c l f") #'eglot-format)
+  (global-set-key (kbd "C-c l h") #'eldoc)
+  (global-set-key (kbd "C-c l i") #'eglot-find-implementation)
+  (global-set-key (kbd "C-c l n") #'eglot-rename)
   (global-set-key (kbd "C-c l q") #'eglot-shutdown)
-  (global-set-key (kbd "C-c l Q") #'eglot-shutdown-all)
-  (global-set-key (kbd "C-c l r") #'eglot-rename))
+  (global-set-key (kbd "C-c l t") #'eglot-find-typeDefinition)
+  (global-set-key (kbd "C-c l R") #'eglot-reconnect)
+  (global-set-key (kbd "C-c l Q") #'eglot-shutdown-all))
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
