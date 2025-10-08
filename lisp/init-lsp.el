211e8ad030991299ce8f3904afb6da32e869206a
dalu <mou.tong@outlook.com>
2025-10-09 02:08:57 +0800

    Update lsp cond

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -7,20 +7,21 @@
 
 ;;; Code:
 
-(global-set-key (kbd "C-c l l") #'eglot)
+(when (fboundp 'eglot)
+  (global-set-key (kbd "C-c l l") #'eglot)
 
-(with-eval-after-load 'eglot
-  (global-set-key (kbd "C-c l a") #'eglot-code-actions)
-  (global-set-key (kbd "C-c l c") #'eglot-show-workspace-configuration)
-  (global-set-key (kbd "C-c l d") #'eglot-find-declaration)
-  (global-set-key (kbd "C-c l f") #'eglot-format)
-  (global-set-key (kbd "C-c l h") #'eldoc)
-  (global-set-key (kbd "C-c l i") #'eglot-find-implementation)
-  (global-set-key (kbd "C-c l n") #'eglot-rename)
-  (global-set-key (kbd "C-c l q") #'eglot-shutdown)
-  (global-set-key (kbd "C-c l t") #'eglot-find-typeDefinition)
-  (global-set-key (kbd "C-c l R") #'eglot-reconnect)
-  (global-set-key (kbd "C-c l Q") #'eglot-shutdown-all))
+  (with-eval-after-load 'eglot
+    (global-set-key (kbd "C-c l a") #'eglot-code-actions)
+    (global-set-key (kbd "C-c l c") #'eglot-show-workspace-configuration)
+    (global-set-key (kbd "C-c l d") #'eglot-find-declaration)
+    (global-set-key (kbd "C-c l f") #'eglot-format)
+    (global-set-key (kbd "C-c l h") #'eldoc)
+    (global-set-key (kbd "C-c l i") #'eglot-find-implementation)
+    (global-set-key (kbd "C-c l n") #'eglot-rename)
+    (global-set-key (kbd "C-c l q") #'eglot-shutdown)
+    (global-set-key (kbd "C-c l t") #'eglot-find-typeDefinition)
+    (global-set-key (kbd "C-c l R") #'eglot-reconnect)
+    (global-set-key (kbd "C-c l Q") #'eglot-shutdown-all)))
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
